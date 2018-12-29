97a9c75f32fe8df29ba751166baab9c673db1bd1
Martin <martinius96@gmail.com>
2018-12-29 22:21:01 +0100

    wpa2

--- wifi_teplomer_wpa2.ino
+++ wifi_teplomer_wpa2.ino
@@ -1,25 +1,19 @@
-//webserver je na IP: 192.168.4.1 --> predpisane kniznicou
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include <OneWire.h>                 //KNIZNICA ONEWIRE PRE VYUZITIE ONEWIRE ZBERNICE
 #include <DallasTemperature.h>       //KNIZNICA PRE TEPLOTNE CIDLA
 #define ONE_WIRE_BUS 2               //DEFINICIA PINU AKO ZBERNICE PRE ONEWIRE ZARIADENIA
 OneWire oneWire(ONE_WIRE_BUS);       //ONEWIRE ČÍTAŤ IBA NA PORTE DEFINOVANOM VYSSIE
 DallasTemperature sensors(&oneWire);
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <SPI.h>
-const char *ssid = "ESPap";
-const char *password = "thereisnospoon";
-
+const char *ssid = "MojaESPsietWPA";
+const char *password = "hesloksieti";
 ESP8266WebServer server(80);
 
-/* IP stranky je 192.168.4.1 (predpísané kniznicou)
-*/
 void handleRoot() {
   sensors.begin(); 
-sensors.requestTemperatures();  
-
-server.send(200, "text/html", "<!DOCTYPE html><html><body><table><tr><td>Cidlo</td><td>Hodnota</td></tr><tr><td>DS18b20 - dnu</td><td>"+(String)sensors.getTempCByIndex(0)+"</td></tr><tr><td>DS18b20 - von</td><td>"+(String)sensors.getTempCByIndex(1)+"</td></tr></table></body></html>");
+  sensors.requestTemperatures();  
+  server.send(200, "text/html", "<!DOCTYPE html><html><body><table><tr><td>Cidlo</td><td>Hodnota</td></tr><tr><td>DS18b20 - dnu</td><td>"+(String)sensors.getTempCByIndex(0)+"</td></tr><tr><td>DS18b20 - von</td><td>"+(String)sensors.getTempCByIndex(1)+"</td></tr></table></body></html>");
 }
 
 void setup() {
@@ -27,15 +21,17 @@ void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.print("Konfigurujem access point...");
-  /* You can remove the password parameter if you want the AP to be open. */
   WiFi.softAP(ssid, password);
-
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("IP adresa pristupoveho bodu je: ");
   Serial.println(myIP);
   server.on("/", handleRoot);
   server.begin();
-  Serial.println("Webserver bezi");
+  Serial.println("Webserver bezi.");
+  Serial.println("Vysiela SSID:");
+  Serial.println(ssid);
+  Serial.println("Heslo WPA2 PSK:");
+  Serial.println(password);
 }
 
 void loop() {
